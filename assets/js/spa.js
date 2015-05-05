d65cfa4f8b24d29c4e887b200b775f2c95ac1522
askl56 <amscott@openmailbox.org>
2015-05-05 07:54:09 +0100

    trying to fix grunt error

---
+++ assets/js/spa.js
@@ -0,0 +1,8 @@
+angular.module('Cossak', []);
+angular.module('Cossak').controller('BaseCtrl', ['$scope', function ($scope){
+
+    $scope.things = [{
+      id: 235935923579
+      name: 'foo'
+    }];
+}]);
