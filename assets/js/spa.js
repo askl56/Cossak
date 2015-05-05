0c5b85f2661f8c17ba3fb8296967d5d003c1c508
askl56 <amscott@openmailbox.org>
2015-05-05 08:39:09 +0100

    trying to get require to work

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -2,7 +2,7 @@ angular.module('Cossak', []);
 angular.module('Cossak').controller('BaseCtrl', ['$scope', function ($scope){
 
     $scope.things = [{
-      id: 235935923579
+      id: 235935923579,
       name: 'foo'
     }];
 }]);
